3963d5e5b39aedaa5ce690c6b1a425063e0f73d1
Oleg <114725982+OlegAksamitny@users.noreply.github.com>
2022-10-21 12:05:24 +0200

    Add files via upload

---
+++ JQuery + @keyframes/js.js
@@ -0,0 +1,18 @@
+const btnJs = document.querySelector('.js');
+function square() {
+    const divItem = document.createElement('div');
+    divItem.classList.add('box')
+    // Albo:
+    // divItem.className = 'box';
+    // Albo:
+    // divItem.setAttribute('class', 'box')
+    document.body.appendChild(divItem);
+    // Albo:
+    // document.body.appendChild(document.createElement('div'));
+}
+btnJs.addEventListener('click', square)
+
+$('.jq').on('click', function () {
+    $('body').append('<div class="box">')
+})
+
