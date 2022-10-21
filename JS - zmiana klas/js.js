5ceb27303e79dad161a4de520fcf538c374c8758
Oleg <114725982+OlegAksamitny@users.noreply.github.com>
2022-10-21 11:41:05 +0200

    Add files via upload

---
+++ JS - zmiana klas/js.js
@@ -0,0 +1,7 @@
+
+const items = document.querySelectorAll('div')
+items.forEach(function(item) {
+    item.addEventListener('click', changeColor)});
+function changeColor() {
+    document.body.className = this.className
+}
