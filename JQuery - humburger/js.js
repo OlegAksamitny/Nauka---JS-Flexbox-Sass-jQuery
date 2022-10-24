3736403d23f3e94f98a6415e31363f60b9361247
Oleg <114725982+OlegAksamitny@users.noreply.github.com>
2022-10-24 11:22:22 +0200

    Add files via upload

---
+++ JQuery - humburger/js.js
@@ -0,0 +1,29 @@
+$('.burger').on('click', function () {
+    $('.fa-burger, .fa-sharp, aside, .wrapper').toggleClass('show')
+})
+
+// const btn = document.querySelector('.burger');
+// const activeElements = document.querySelectorAll('.active');
+// function menu() {
+//     for (let i = 0; i < activeElements.length; i++) {
+//         activeElements[i].classList.toggle('show')
+//     }
+// }
+// btn.addEventListener('click', menu)
+
+
+// const btn = document.querySelector('.burger');
+// const iconBurger = document.querySelector('.fa-burger');
+// const iconX = document.querySelector('.fa-sharp');
+// const column = document.querySelector('aside')
+// const wrapper = document.querySelector('.wrapper')
+
+// function menu() {
+//     iconBurger.classList.toggle('show')
+//     iconX.classList.toggle('show')
+//     column.classList.toggle('show')
+//     wrapper.classList.toggle('show')
+
+//     console.log('dziala')
+// }
+// btn.addEventListener('click', menu)
